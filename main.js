5c32b3887087c3198b4da96467a1333eb52aae7b
hammibell <94499945+hammibell@users.noreply.github.com>
2023-03-26 16:29:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function bedroom(){
+window.location.href=("bedroom.html");
+}
+function tv(){
+window.location("tv.html");
+}
+function desk(){
+window.location("desk.html");
+}
+function bottles(){
+window.location("bottles.html");
+}
+
+array = "";
+
+function draw(){
+    if("status" != ""){
+        for (i = 0; i < Object; i++){
+            (objects[i].confidence)*100;
+            document.getElementById("label").value;
+            obkects[i].x;
+            objects[i].y;
+            objects[i].width;
+            objects[i].height;
+         
+    
+    }
+}
+
+}
